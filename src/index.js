eac2f6192a6b166a02b52b5df53f707f863ad3cd
Miguel Khatounian Filho <miguelktnf@gmail.com>
2022-06-20 11:15:21 -0300

    added drawing metrics and minor changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("gameScreen")
 const ctx = canvas.getContext("2d")
+const slider = document.getElementById("FPSValue")
 
 canvasDimensions = {
     width: 500,
@@ -23,22 +24,15 @@ let birdQty = 50
 let frameCounts = 0
 let aliveBirds = birdQty
 let currentGeneration = 0
-
-let birds = generateBirds(birdQty)
+let FPS = 60
+let birds = generateFirstGeneration(birdQty)
 
 animate()
-function animate() {
 
+function animate() {
+    
     if (aliveBirds == 0) {
-        currentGeneration += 1
-        console.log(
-            'Currente Gen:', currentGeneration,
-            'Best Bird:', Math.max(...birds.map(o => o.fitness))
-        )
-        aliveBirds = birdQty
-        pipes = []
-        frameCounts = 0 
-        birds = nextGeneration(birds = birds, numOfParents = 5, mutationAmount = 0.4)
+        startNewGeneration()
     }
 
     // add new pipe
@@ -49,8 +43,7 @@ function animate() {
     // update pipes
     for (let i = 0; i < pipes.length; i++) {
         pipes[i].update()
-
-        if (pipes[i].x + pipes[i].width < 0) {
+        if (pipes[i].offScreen()) {
             pipes.splice(i, 1)
             i--
         }
@@ -62,18 +55,17 @@ function animate() {
             birds[i].update(pipes, canvasDimensions, baseDimensions)
             birds[i].think(pipes)
             if (birds[i].isDamaged) {
-                // birds.splice(i, 1)
-                aliveBirds -= 1
+                aliveBirds--
+                console.log(birds[i].fitness)
             }
         }
-
     }
 
     // draw everything
     ctx.clearRect(0, 0, canvasDimensions.width, canvasDimensions.height)
     drawBackground()
     for (let bird of birds) {
-        if (!bird.isDamaged){
+        if (!bird.isDamaged) {
             bird.draw(ctx)
         }
     }
@@ -82,11 +74,42 @@ function animate() {
     }
     drawbase()
 
-    frameCounts += 1
-    setTimeout(animate, 1000 / 240)
+    ctx.font = '30px Open Sans'
+    ctx.fillText(
+        'Score ' + Math.max(...birds.map(o => o.fitness)).toLocaleString(),
+        canvasDimensions.width/20, 50
+    )
+
+    ctx.fillText(
+        'Alive ' + aliveBirds + ' / ' + birdQty,
+        canvasDimensions.width/20, 90
+    )
+
+    ctx.fillText(
+        'Gen ' + currentGeneration,
+        canvasDimensions.width/20, 130
+    )
+
+    frameCounts++
+
+    //get fps value from slider
+    FPS = parseInt(slider.value)
+    setTimeout(animate, 1000 / FPS)
 }
 
-function generateBirds(birdQty) {
+function startNewGeneration() {
+    // console.log(
+    //     'Generation:', currentGeneration,
+    //     'Best Bird:', Math.max(...birds.map(o => o.fitness))
+    // )
+    currentGeneration += 1
+    aliveBirds = birdQty
+    pipes = []
+    frameCounts = 0
+    birds = GA.nextGeneration(birds = birds, numOfParents = 2, mutationAmount = 0.2)
+}
+
+function generateFirstGeneration(birdQty) {
     let birds = []
     for (let i = 0; i < birdQty; i++) {
         birds[i] = new Bird(x = 100, width = 40, height = 30)
