43fcdac5bb716485de8f085a8254e4815cf292e6
Miguel Khatounian Filho <miguelktnf@gmail.com>
2022-06-20 11:12:23 -0300

    fixed nextgeneration function and made it an class

--- src/ga.js
+++ src/ga.js
@@ -1,46 +1,66 @@
-function mutate(bird, amount = 1) {
-    let birdCopy = bird.copy()
-    amount = amount < 0 ? 0 : amount
-    amount = amount > 1 ? 1 : amount
+class GA {
 
-    for (let level of birdCopy.brain.levels) {
-        for (let i = 0; i < level.biases.length; i++) {
-            level.biases[i] = level.biases[i] + (Math.random() * 2 - 1 - level.biases[i]) * amount
-        }
-        for (let i = 0; i < level.weights.length; i++) {
-            for (let j = 0; j < level.weights[i].length; j++) {
-                level.weights[i][j] = level.weights[i][j] + (Math.random() * 2 - 1 - level.weights[i][j]) * amount
+    static mutate(bird, amount = 1) {
+        let birdCopy = bird.copy()
+        amount = amount < 0 ? 0 : amount
+        amount = amount > 1 ? 1 : amount
+
+        for (let level of birdCopy.brain.levels) {
+            for (let i = 0; i < level.biases.length; i++) {
+                level.biases[i] = level.biases[i] + (Math.random() * 2 - 1 - level.biases[i]) * amount
+            }
+            for (let i = 0; i < level.weights.length; i++) {
+                for (let j = 0; j < level.weights[i].length; j++) {
+                    level.weights[i][j] = level.weights[i][j] + (Math.random() * 2 - 1 - level.weights[i][j]) * amount
+                }
             }
         }
-    }
-    return birdCopy
-}
-
-function selectParents(birds, numOfParents) {
-    // let birdsCopy = []
-    // for (bird for birds) {
-    //     birdsCopy = bird.copy()
-    // }
-    birds.sort(function (a, b) {
-        return parseInt(b.fitness) - parseInt(a.fitness)
-    })
-    return birds.slice(0, numOfParents)
-}
-
-function nextGeneration(birds, numOfParents, mutationAmount) {
-    let newBirds = []
-    let parents = selectParents(birds, numOfParents)
-
-    for (let i = 0; i < numOfParents; i++) {
-        newBirds.push(parents[i].copy())
+        return birdCopy
     }
 
-    for (let i = numOfParents; i < birds.length; i++) {
-        randomParent = parents[Math.floor(Math.random() * parents.length)]
-        newBirds.push(mutate(bird = randomParent.copy(), amount = mutationAmount))
-        newBirds[i] = randomParent.copy()
-        newBirds[i] = mutate(newBirds[i])
+    static selectParents(birds, numOfParents) {
+        birds.sort(function (a, b) {
+            return parseInt(b.fitness) - parseInt(a.fitness)
+        })
+        return birds.slice(0, numOfParents)
     }
 
-    return newBirds
+    static nextGeneration(birds, numOfParents, mutationAmount) {
+        let newBirds = []
+        let parents = this.selectParents(birds, numOfParents)
+
+        for (let i = 0; i < numOfParents; i++) {
+            newBirds[i] = parents[i].copy()
+        }
+
+        for (let i = numOfParents; i < birds.length; i++) {
+            let randomParent = parents[Math.floor(Math.random() * parents.length)]
+            let child = randomParent.copy()
+            child = this.mutate(child, mutationAmount)
+            newBirds[i] = child
+        }
+
+        return newBirds
+    }
+
 }
+
+
+// let birds = []
+// birds[0] = new Bird(fitness=12)
+// birds[1] = new Bird(fitness=20)
+// birds[2] = new Bird(fitness=5)
+// birds[3] = new Bird(fitness=100)
+
+
+// console.log('---------')
+// for (let bird of birds){
+//     console.log(bird.brain.levels[0].weights)
+// }
+
+// let nextGen = GA.nextGeneration(birds, 2, 0.1)
+
+// console.log('---------')
+// for (let bird of nextGen){
+//     console.log(bird.brain.levels[0].weights)
+// }
